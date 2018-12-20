6ece76fb883f5235416f9840c378b41ac442d6a6
wyf1122 <1104870198@qq.com>
2018-12-20 23:55:44 +0800

    cehsi

--- server/control/fileControl.js
+++ server/control/fileControl.js
@@ -11,10 +11,11 @@ router.get('/_handel', function (req, res) {
 });
 
 router.post('/_handel', function (req, res) {
-    
-    var urlData = JSON.parse(req.body.data);
-    var url = JSON.parse(req.body);
 
+    var urlData = JSON.parse(req.body);
+    console.log(urlData.url, urlData.data);
+    // console.log();
+    
     console.log(urlData, url);
     res.send({code: 0, data: "成功访问回调post接口"})
 });
