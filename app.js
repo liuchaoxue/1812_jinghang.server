633459df36d567b00d26756b4a28bd2362727243
liguangsong <863925367@qq.com>
2019-01-11 20:28:29 +0800

    设置接受headers值

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var app = express();
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     next();
 });
