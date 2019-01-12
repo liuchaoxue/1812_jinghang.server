ad454ac5709abbccbd3715401f97d6ed385bec8a
liguangsong <863925367@qq.com>
2019-01-12 18:46:52 +0800

    更新文件

--- test.js
+++ test.js
@@ -1,11 +1,73 @@
-var ffmpeg = require('fluent-ffmpeg');
+// var ffmpeg = require('fluent-ffmpeg');
 var fs = require('fs')
+var Path =  require('path');
+var basePath = Path.resolve('../media/')
+
+var uploadPath = Path.resolve('../media/upload');
+
+var ewaPath = Path.resolve('../media/ewa');
+var tedPath = Path.resolve('../media/ted');
+var bbcPath = Path.resolve('../media/bbc');
+
+var ewaFiles = fs.readdirSync(ewaPath);
+
+var uploadFiles = fs.readdirSync(uploadPath);
+var tedFiles = fs.readdirSync(tedPath);
+var bbcFiles= fs.readdirSync(bbcPath);
+
+uploadFiles.forEach(function(ele,index){
+    let path = Path.join(uploadPath, ele)
+    let filePath =  Path.join( basePath, ele.split('-')[0]);
+    if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath);
+    }
+     fs.rename(path, Path.join(filePath, ele.split('.')[0]),(err)=>{
+         if(!err) return console.log(Path.join(filePath, ele.split('.')[0]))
+         console.log(err)
+     })
+})
+ewaFiles.forEach(function(ele,index){
+    let path = Path.join(ewaPath, ele)
+    let filePath =  Path.join( basePath, ele.split('-')[0]);
+    if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath);
+    }
+
+    fs.rename(path, Path.join(filePath, ele.split('.')[0]),(err)=>{
+        if(!err) return console.log(Path.join(filePath, ele.split('.')[0]))
+        console.log(err)
+    })
+})
+tedFiles.forEach(function(ele,index){
+    let path = Path.join(tedPath, ele)
+    let filePath =  Path.join( basePath, ele.split('-')[0]);
+    if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath);
+    }
+    fs.rename(path, Path.join(filePath, ele.split('.')[0]),(err)=>{
+        if(!err) return console.log(Path.join(filePath, ele.split('.')[0]))
+        console.log(err)
+    })
+})
+bbcFiles.forEach(function(ele,index){
+    let path = Path.join(bbcPath, ele)
+    let filePath =  Path.join( basePath, ele.split('-')[0]);
+    if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath);
+    }
+    fs.rename(path, Path.join(filePath, ele.split('.')[0]),(err)=>{
+        if(!err) return console.log(Path.join(filePath, ele.split('.')[0]))
+        console.log(err)
+    })
+})
+
+
 //
 
-fs.stat('/home/lgs/work/media/04c7ec3e/04c7ec3e-79ca-4136-b588-e58032376730', function(err,data){
-    console.log(err)
-    console.log(data)
-})
+// fs.stat('/home/lgs/work/media/04c7ec3e/04c7ec3e-79ca-4136-b588-e58032376730', function(err,data){
+//     console.log(err)
+//     console.log(data)
+// })
 // var a =ffmpeg('/home/lgs/work/media/04c7ec3e/04c7ec3e-79ca-4136-b588-e58032376730');
 //     a.setStartTime('00:00:00')
 //     .setDuration('5')
